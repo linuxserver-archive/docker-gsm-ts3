a82558a58ac791628a39bb559c09e45fe4772843
Stian Buch Larsen <lonixx@gmail.com>
2015-09-09 10:58:41 +0200

    spesifying output file og wget command

--- init/20_install_gsm.sh
+++ init/20_install_gsm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chown abc /config
+
 if [ "$(grep abc /etc/passwd | cut -d ":" -f6)" != "/config" ]; then usermod -d /config abc; fi 
-if [ ! -f /config/ts3server ]; then /sbin/setuser abc wget http://gameservermanagers.com/dl/ts3server; fi
-if [ ! -x /config/ts3server ]; then /sbin/setuser abc chmod +x /conig/ts3server; fi
+if [ ! -f /config/ts3server ]; then /sbin/setuser abc wget -O /config/ts3server http://gameservermanagers.com/dl/ts3server; fi
+if [ ! -x /config/ts3server ]; then /sbin/setuser abc chmod +x /config/ts3server; fi
 if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /sbin/setuser abc /config/ts3server auto-install; fi
