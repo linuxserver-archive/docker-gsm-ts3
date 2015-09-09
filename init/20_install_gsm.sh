3edfd64cc692d7eaf4194ae967cf7965bf4a8af0
Stian Buch Larsen <lonixx@gmail.com>
2015-09-09 10:53:38 +0200

    Fixed UUOC

--- init/20_install_gsm.sh
+++ init/20_install_gsm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-if [ $(cat /etc/passwd | grep abc | cut -c":" -f6) != "/config" ]; then usermod -d /config abc; fi 
+chown abc /config
+if [ "$(grep abc /etc/passwd | cut -d ":" -f6)" != "/config" ]; then usermod -d /config abc; fi 
 if [ ! -f /config/ts3server ]; then /sbin/setuser abc wget http://gameservermanagers.com/dl/ts3server; fi
 if [ ! -x /config/ts3server ]; then /sbin/setuser abc chmod +x /conig/ts3server; fi
 if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /sbin/setuser abc /config/ts3server auto-install; fi
