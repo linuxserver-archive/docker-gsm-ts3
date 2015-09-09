94badd84e81e276f0c57a0274c085e71fe79a1b7
Stian Buch Larsen <lonixx@gmail.com>
2015-09-09 10:44:11 +0200

    Removeing "Confuseing alias"

--- init/20_install_gsm.sh
+++ init/20_install_gsm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-alias abc="/sbin/setuser abc"
 
 if [ $(cat /etc/passwd | grep abc | cut -c":" -f6) != "/config" ]; then usermod -d /config abc; fi 
 if [ ! -f /config/ts3server ]; then /sbin/setuser abc wget http://gameservermanagers.com/dl/ts3server; fi
