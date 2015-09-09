313a40b82878ef14b8f3a82bcc473540b33cf485
sparklyballs <sparklyballs@gmail.com>
2015-09-09 21:56:39 +0100

    remove usermod from init and condense if statement

--- init/20_install_gsm.sh
+++ init/20_install_gsm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ "$(grep abc /etc/passwd | cut -d ":" -f6)" != "/config" ]; then usermod -d /config abc; fi 
-if [ ! -f /config/ts3server ]; then /sbin/setuser abc wget -O /config/ts3server http://gameservermanagers.com/dl/ts3server; fi
-if [ ! -x /config/ts3server ]; then /sbin/setuser abc chmod +x /config/ts3server; fi
+if [ ! -f /config/ts3server ]; then
+/sbin/setuser abc wget -O /config/ts3server http://gameservermanagers.com/dl/ts3server
+/sbin/setuser abc chmod +x /config/ts3server
+fi
+
 if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /sbin/setuser abc /config/ts3server auto-install; fi
