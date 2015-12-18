11c922677cadcbf7f338b9a1a3f82afd03680895
sparklyballs <sparklyballs@gmail.com>
2015-12-18 09:18:28 +0000

    fix for non unraid hosts

--- init/30_install_gsm.sh
+++ init/30_install_gsm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+chown abc:abc /config
+
 if [ ! -f /config/ts3server ]; then
 /sbin/setuser abc wget -O /config/ts3server http://gameservermanagers.com/dl/ts3server
 /sbin/setuser abc chmod +x /config/ts3server
 fi
 
-if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /config/ts3server auto-install && chown abc:abc -R /config/* ; fi
+if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /sbin/setuser abc /config/ts3server auto-install ; fi
+
