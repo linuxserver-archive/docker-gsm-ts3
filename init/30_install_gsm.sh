1e8ec1cb6aab10642d7aa6a9d5e49e278ddf8e33
Stian Buch Larsen <lonixx@gmail.com>
2015-12-18 10:07:20 +0100

    Fx for non unraid os's

--- init/30_install_gsm.sh
+++ init/30_install_gsm.sh
@@ -5,4 +5,4 @@ if [ ! -f /config/ts3server ]; then
 /sbin/setuser abc chmod +x /config/ts3server
 fi
 
-if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /sbin/setuser abc /config/ts3server auto-install; fi
+if [ ! -f /config/serverfiles/ts3server_minimal_runscript.sh ]; then /config/ts3server auto-install && chown abc:abc -R /config/* ; fi
